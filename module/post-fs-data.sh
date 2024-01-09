5003981ad63efe3e5c78f96e526cb314944b7ad9
imoc <wzj9912@icloud.com>
2024-01-09 10:53:28 +0800

    Init

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -37,3 +37,44 @@ resetprop_if_diff ro.is_ever_orange 0
 resetprop_if_diff ro.build.type user
 resetprop_if_diff ro.debuggable 0
 resetprop_if_diff ro.secure 1
+
+modelprops="ro.product.model
+ro.product.bootimage.model
+ro.product.odm.model
+ro.product.product.model
+ro.product.system.model
+ro.product.system_ext.model
+ro.product.vendor.model
+ro.product.vendor_dlkm.model"
+for propname in $modelprops
+do
+    resetprop $propname "ASUS_AI2201_F"
+    resetprop -n $propname "ASUS_AI2201_F"
+    resetprop -p $propname "ASUS_AI2201_F"
+    resetprop -n $propname "ASUS_AI2201_F"
+    resetprop $propname "ASUS_AI2201_F"
+done
+
+MODDIR=${0%/*}
+LOGFILE=$MODDIR/pfd.log
+
+rm -rf $LOGFILE
+cat $MODDIR/system.prop.persist > $MODDIR/system.prop
+
+for CP in $CNPROPLISTFiltered
+do
+  OG="`getprop $CP`"
+  TG="`echo "$OG" | sed 's/CN/WW/g'`"
+  echo "$CP changed from \"$OG\" to \"$TG\"" >>$LOGFILE
+  resetprop $CP "$TG" 2>&1 >>$LOGFILE
+  echo "$CP=$TG" >> $MODDIR/system.prop
+done
+
+sh $MODDIR/alter-model.sh $MODDIR $LOGFILE
+
+resetprop ro.boot.image.valid "Y"
+resetprop vendor.asus.image.valid "Y"
+setprop ro.boot.image.valid "Y"
+setprop vendor.asus.image.valid "Y"
+
+echo "`date`: done" >> $LOGFILE
--- module/service.sh
+++ module/service.sh
@@ -32,6 +32,48 @@ if [ "$(toybox cat /sys/fs/selinux/enforce)" == "0" ]; then
     chmod 440 /sys/fs/selinux/policy
 fi
 
+modelprops="ro.product.model
+ro.product.bootimage.model
+ro.product.odm.model
+ro.product.product.model
+ro.product.system.model
+ro.product.system_ext.model
+ro.product.vendor.model
+ro.product.vendor_dlkm.model"
+for propname in $modelprops
+do
+    resetprop $propname "ASUS_AI2201_F"
+    resetprop -n $propname "ASUS_AI2201_F"
+    resetprop -p $propname "ASUS_AI2201_F"
+    resetprop -n $propname "ASUS_AI2201_F"
+    resetprop $propname "ASUS_AI2201_F"
+done
+
+
+MODDIR=${0%/*}
+LOGFILE=$MODDIR/s.log
+rm -rf $LOGFILE
+
+CNPROPLISTR="`getprop | grep CN | grep -Fe "[CN]" -e "CN_AI2201" -e "release-keys"`"
+CNPROPLISTFiltered="`echo "$CNPROPLISTR" | grep -ve vendor.asus.operator.iso-country -e persist.vendor.asus.ship_location -e persist.vendor.fota | awk -F '[' '{print $2}' | awk -F ']' '{print $1}'`"
+
+for CP in $CNPROPLISTFiltered
+do
+  OG="`getprop $CP`"
+  TG="`echo "$OG" | sed 's/CN/WW/g'`"
+  echo "$CP changed from \"$OG\" to \"$TG\"" >>$LOGFILE
+  resetprop $CP "$TG" 2>&1 >>$LOGFILE
+  echo "$CP=$TG" >> $MODDIR/system.prop
+done
+
+sh $MODDIR/alter-model.sh $MODDIR $LOGFILE
+
+resetprop ro.boot.image.valid "Y"
+resetprop vendor.asus.image.valid "Y"
+setprop ro.boot.image.valid "Y"
+setprop vendor.asus.image.valid "Y"
+
+
 # late props which must be set after boot_completed for various OEMs
 until [ "$(resetprop sys.boot_completed)" == "1" ]; do
 	sleep 1
